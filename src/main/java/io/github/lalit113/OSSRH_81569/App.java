405e1903c3cf70e9e5988e8e37dec903cccefa9e
lalit113 <lalit24899@gmail.com>
2022-06-09 10:36:38 +0530

    Publish Jar to Maven Central

---
+++ src/main/java/io/github/lalit113/OSSRH_81569/App.java
@@ -0,0 +1,13 @@
+package io.github.lalit113.OSSRH_81569;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
